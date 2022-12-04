38ddce5c9dfd7a14f595f6318b8002b41a007437
Willem Verbeke <willem_verbeke@hotmail.com>
2022-12-04 14:32:10 +0100

    Make a vector class and write it to a h5 file as arrays

---
+++ test_vector.cpp
@@ -0,0 +1,60 @@
+#include "hdf5.h"
+#include <string>
+#include <vector>
+
+class Vec{
+
+    public:
+        Vec(): _x(0.0), _y(0.0), _z(0.0) {}
+        Vec(double x, double y, double z): _x(x), _y(y), _z(z) {}
+
+        double x() const{ return _x; }
+        double y() const{ return _y; }
+        double z() const{ return _z; }
+
+        //std::vector<double> to_vector() const{ return std::vector<double>{_x, _y, _z}; }
+
+    private:
+        double _x;
+        double _y;
+        double _z;
+};
+
+
+void write_vecs(const std::string& filename, const std::string& dset_name, const std::vector<Vec>& vecs){
+
+
+    // Each vector has 3 elements.
+    const hsize_t dims[2] = {vecs.size(), 3};
+    
+    // Convert the vectors to a 2D arr
+    //const double vec_array[
+    //std::vector<std::vector<double>> vec_array(
+    double vec_array[vecs.size()][3];
+    for(size_t i=0; i < vecs.size(); ++i){
+        vec_array[i][0] = vecs[i].x();
+        vec_array[i][1] = vecs[i].y();
+        vec_array[i][2] = vecs[i].z();
+    }
+
+    hid_t file_id = H5Fcreate(filename.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    hid_t dataspace_id = H5Screate_simple(2, dims, NULL);
+    hid_t dataset_id = H5Dcreate(file_id, dset_name.c_str(), H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    
+    herr_t status = H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, vec_array);
+    status = H5Dclose(dataset_id);
+    status = H5Sclose(dataspace_id);
+    status = H5Fclose(file_id);
+}
+
+
+int main(){
+
+    std::vector<Vec> vecs_to_write;
+    for(unsigned i=0; i < 100; ++i){
+        vecs_to_write.push_back(Vec{static_cast<double>(i), static_cast<double>(i), static_cast<double>(i)});
+    }
+    write_vecs("vec_file.h5", "vectors", vecs_to_write);
+    
+    return 0;
+}
